e6ec808b65ce41fbb5743504007148098f189958
Keigo Habara <k.habara.aa@gmail.com>
2018-07-14 20:54:17 +0900

    add sample class directory

---
+++ class/Game.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "Game.h"
+
+void Game::show() {
+  std::cout << p.get_color() << std::endl;
+}
---
+++ class/Game.h
@@ -0,0 +1,12 @@
+#ifndef _INC_GAME_
+#define _INC_GAME_
+#include <iostream>
+#include "Player.h"
+
+class Game {
+  private:
+    Player p;
+  public:
+    void show();
+};
+#endif

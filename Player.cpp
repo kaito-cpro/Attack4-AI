b6f56b2d47398338175412b89661893626210434
kaito <39236776+kaito-cpro@users.noreply.github.com>
2018-07-26 13:00:30 +0900

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,8 +1,2 @@
-#include "define.hpp"
-#include "Game.hpp"
-#include "Board.hpp"
-#include "Player.hpp"
-#include "extern.hpp"
-
-void Player::put_stone(int x){
-};
+#include "../hdr/common.h"
+#include "../hdr/Player.h"
